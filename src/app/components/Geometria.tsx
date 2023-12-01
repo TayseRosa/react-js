feb212d63ec27cdced0f8bffce8a61fb8614fac8
Tayse Rosa <tayse_rosa@yahoo.com.br>
2023-12-01 06:10:45 -0300

    First commit

---
+++ src/app/components/Geometria.tsx
@@ -0,0 +1,15 @@
+export const Square = () => {
+    return(
+        <div className="w-52 h-52 bg-orange-700 text-white">
+            Texto qualquer inspirador
+        </div>
+    )
+}
+
+export const Circle = () => {
+    return(
+        <div className="w-52 h-52 rounded-full bg-yellow-700 text-white">
+            Texto qualquer inspirador
+        </div>
+    )
+}
