82e3578cbfe8ef021e0a9cf920d1f9504cfd1287
Tayse Rosa <tayse_rosa@yahoo.com.br>
2023-12-09 09:05:44 -0300

    State Updater

--- src/app/5_page_usandoStateEmCampos.tsx
+++ src/app/5_page_usandoStateEmCampos.tsx
@@ -3,21 +3,23 @@
 import { useState } from "react";
 
 const Page = () => {
-
-  const [count, setCount] = useState(0)
+  const [ nameInput, setNameInput ] = useState('');
 
   const handleClick = () => {
-    setCount(count + 2)
-    setCount( c => c + 2 )
-    setCount( c => c + 2 )
+    setNameInput(nameInput)
   }
   
   return(
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
       
-      <p> {count} </p>
-      <button onClick={handleClick} className="bg-blue-200 text-white p-3 rounded-md"> +2 </button>
+      <input 
+        type="text"  
+        className="border-black p-3 text-xl text-black rounded"
+        placeholder="Digite seu nome"
+        onChange={e=>setNameInput(e.target.value)}
+      />
 
+      <p>Typed name: {nameInput}</p>
 
       <div className="w-screen flex flex-row justify-center items-center mt-40 text-2xl text-purple-400"> Developed with 💜 by <a href="https://www.github.com/tayserosa" target="_blank"> <img src="https://github.com/tayserosa.png" width={40} alt="Tayse CODE Rosa"className="ml-2" /> </a> </div>
     </div>
