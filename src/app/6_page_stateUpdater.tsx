d6b6811494b7befcd5192c66ea08b56143502000
Tayse Rosa <tayse_rosa@yahoo.com.br>
2023-12-11 13:14:02 -0300

    Atualizando Objetos em States

--- src/app/6_page_stateUpdater.tsx
+++ src/app/6_page_stateUpdater.tsx
@@ -3,24 +3,22 @@
 import { useState } from "react";
 
 const Page = () => {
+
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => {
+    setCount(count + 2)
+    setCount( c => c + 2 )
+    setCount( c => c + 2 )
+  }
   
   return(
     <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
-      <input 
-        type="text"
-        placeholder="Name"
-        className="border border-black p-3 text-2xl text-black rouded-md mb-3"
-      />
-
-      <input 
-        type="text"
-        placeholder="Last name"
-        className="border border-black p-3 text-2xl text-black rouded-md mb-3"
-      />
-
-      <p>My full name is:</p>
-      <p>...</p>
       
+      <p> {count} </p>
+      <button onClick={handleClick} className="bg-blue-200 text-white p-3 rounded-md"> +2 </button>
+
+
       <div className="w-screen flex flex-row justify-center items-center mt-40 text-2xl text-purple-400"> Developed with 💜 by <a href="https://www.github.com/tayserosa" target="_blank"> <img src="https://github.com/tayserosa.png" width={40} alt="Tayse CODE Rosa"className="ml-2" /> </a> </div>
     </div>
   );
