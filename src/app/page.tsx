25711328d759a6fc3345be9537368ef94efb7b4e
Tayse Rosa <tayse_rosa@yahoo.com.br>
2023-12-29 20:21:24 -0300

    Definindo um effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,30 @@
 "use client"
+
+import { useEffect, useState } from "react";
+
 const Page = () => {
-  const title = 'Effect - React JS';
+
+  const [name, setName] = useState('Tayse Rosa')
+
+  useEffect( ()=>{
+    console.log('RODOU O EFFECT...')
+  },[name] );
+
+  const handleChangeButton = () => {
+    setName('Maria Tayse')
+  }
+
+  const handleChangeButton2 = () => {
+    setName('Code Maria')
+  }
+
 
   return(
     <div className="w-full h-screen flex justify-center items-center bg-purple-400">
-      <div className="w-full max-w-xl rounded rounded-xl bg-white text-black shadow shadow-black">
-        <div className="p-5 font-bold text-2-xl border border-gray-300"> {title} </div>
+      <p>My name is {name}</p>
 
-        <div className="flex justify-center mt-10 mb-10 mx-4 text-2md text-purple-400"> 
-          <a href="https://www.github.com/tayserosa" target="_blank"> 
-            Developed with 💜 by  Tayse Rosa
-          </a> 
-        </div>
-      </div>
+      <button onClick={handleChangeButton} className="py-4 px-5 rounded-md bg-slate-600" >Mudar para Maria Tayse</button>
+      <button onClick={handleChangeButton2} className="py-4 px-5 rounded-md bg-slate-600" >Mudar para Code Maria</button>
     </div>
   );
 }
