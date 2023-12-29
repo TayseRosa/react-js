e424925a3aba9f71a8b3c114c19faa7036cd5c59
Tayse Rosa <tayse_rosa@yahoo.com.br>
2023-12-29 12:06:04 -0300

    Quiz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,16 @@
 
 import { questions } from "@/data/questions";
 import { useState } from "react";
+import { QuestionItem } from "./components/QuestionItem";
 
 const Page = () => {
 
   const [ currentQuestion, setCurrentQuestion ] = useState(0)
   const title = 'Quiz de culinária'
+
+  const handleAnswered = ( answer:number ) => {
+
+  }
 
   return(
     <div className="w-full h-screen flex justify-center items-center bg-blue-600">
@@ -15,7 +20,13 @@ const Page = () => {
         
         <div className="p-5 font-bold text-2-xl border border-gray-300"> {title} </div>
 
-        <div className="p-5"> ... </div>
+        <div className="p-5"> 
+          <QuestionItem 
+            question={questions[currentQuestion]}
+            count={currentQuestion + 1}
+            onAnswer={handleAnswered}
+          />
+        </div>
 
         <div className="p-5 text-center border-t border-gray-300"> {currentQuestion + 1} de {questions.length} pergunta{questions.length === 1 ? '' : 's' } </div>
 
